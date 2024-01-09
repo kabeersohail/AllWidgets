fd230ccfad3e221b90fdb63f45835c28901a6bd3
KabeerSohail <sohail.mohammad@wenable.com>
2024-01-09 19:51:25 +0530

    1. Added data binding 2. Added click listeners

--- AllWidgets/app/src/main/java/com/chirput/allwidgets/MainActivity.kt
+++ AllWidgets/app/src/main/java/com/chirput/allwidgets/MainActivity.kt
@@ -2,10 +2,21 @@ package com.chirput.allwidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.chirput.allwidgets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.fetchAllWidgets.setOnClickListener {
+            Toast.makeText(this, "Fetching widgets", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
