7cbaa0faa4cbce50be2a9177fb255041c2d54714
KabeerSohail <sohail.mohammad@wenable.com>
2024-01-09 20:17:03 +0530

    logging all widgets in logcat

--- AllWidgets/app/src/main/java/com/chirput/allwidgets/MainActivity.kt
+++ AllWidgets/app/src/main/java/com/chirput/allwidgets/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.chirput.allwidgets
 
-import androidx.appcompat.app.AppCompatActivity
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProviderInfo
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.chirput.allwidgets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appWidgetManager: AppWidgetManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        appWidgetManager = getSystemService(APPWIDGET_SERVICE) as AppWidgetManager
 
         binding.fetchAllWidgets.setOnClickListener {
             Toast.makeText(this, "Fetching widgets", Toast.LENGTH_SHORT).show()
+            populateWidgets()
         }
-
     }
+
+    private fun populateWidgets() {
+        val providers: List<AppWidgetProviderInfo> = appWidgetManager.installedProviders
+
+        for(info in providers) {
+            Log.d("SOHAIL_BRO", "$info")
+        }
+    }
+
 }
