332212602ee4e664bedeed9ee54b7270436a817d
KabeerSohail <sohail.mohammad@wenable.com>
2024-01-10 20:55:05 +0530

    widget showing up but action buttons are not working

--- AllWidgets/app/src/main/java/com/chirput/allwidgets/MainActivity.kt
+++ AllWidgets/app/src/main/java/com/chirput/allwidgets/MainActivity.kt
@@ -4,7 +4,6 @@ import android.appwidget.AppWidgetHost
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProviderInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.chirput.allwidgets.databinding.ActivityMainBinding
@@ -16,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var appWidgetHost: AppWidgetHost
     private lateinit var appWidgetManager: AppWidgetManager
+    private var newAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
 
     private val addWidgetLauncher = registerForActivityResult<Int, AppWidgetProviderInfo>(
         WidgetGalleryActivity.SelectContract()
@@ -37,12 +37,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startAddNewWidget() {
-        val newAppWidgetId = appWidgetHost.allocateAppWidgetId()
+        newAppWidgetId = appWidgetHost.allocateAppWidgetId()
         addWidgetLauncher.launch(newAppWidgetId)
     }
 
     private fun onNewWidgetSelected(info: AppWidgetProviderInfo?) {
-        Log.d("here", "")
+        if (info != null) {
+            addNewWidget(info)
+        } else {
+            cancelAddNewWidget(newAppWidgetId)
+        }
+    }
+
+    private fun addNewWidget(info: AppWidgetProviderInfo) {
+        val widgetView = appWidgetHost.createView(
+            this,
+            APP_WIDGET_HOST_ID,
+            info
+        ) ?: return
+
+        val widgetContainer = binding.widgetContainer
+        widgetContainer.addView(widgetView)
+    }
+
+    private fun cancelAddNewWidget(appWidgetId: Int) {
+        if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            return
+        }
+        appWidgetHost.deleteAppWidgetId(newAppWidgetId)
+        newAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
     }
 
     override fun onDestroy() {
