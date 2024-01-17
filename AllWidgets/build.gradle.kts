62b7234482c4560d27ac6cf8a6ed743944117c40
KabeerSohail <sohail.mohammad@wenable.com>
2024-01-18 02:00:13 +0530

    renamed to com.android.launcher3

--- AllWidgets/build.gradle.kts
+++ AllWidgets/build.gradle.kts
@@ -1,4 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+allprojects {
+    
+}
+
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
--- AllWidgets/settings.gradle.kts
+++ AllWidgets/settings.gradle.kts
@@ -15,4 +15,28 @@ dependencyResolutionManagement {
 
 rootProject.name = "AllWidgets"
 include(":app")
+
+include(":iconloaderlib")
+project(":iconloaderlib").projectDir = File(rootDir, "platform_frameworks_libs_systemui/iconloaderlib")
+
+include(":searchuilib")
+project(":searchuilib").projectDir = File(rootDir, "platform_frameworks_libs_systemui/searchuilib")
+
+include(":animationlib")
+project(":animationlib").projectDir = File(rootDir, "platform_frameworks_libs_systemui/animationlib")
+
+include(":hidden-api")
+include(":systemUIShared")
+include(":systemUIPlugin")
+include(":systemUIPluginCore")
+include(":systemUICommon")
+include(":systemUILog")
+include(":systemUIAnim")
+include(":systemUnFold")
+include(":systemUIViewCapture")
+include(":compatLib")
+include(":compatLibVR")
+include(":compatLibVS")
+include(":baseline-profile")
+
  
