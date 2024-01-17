8cff7d9008ec89dbcd5a7c749522f4b244753ee4
KabeerSohail <sohail.mohammad@wenable.com>
2024-01-18 02:11:35 +0530

    added resizable widgets

---
+++ AllWidgets/app/src/main/java/com/chirput/allwidgets/ResizableFrameLayout.kt
@@ -0,0 +1,150 @@
+package com.chirput.allwidgets
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.FrameLayout
+
+class ResizableFrameLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val TAG = "YourTouchListener"
+    private var startX = 0f
+    private var startY = 0f
+
+    private var isLeftEdge: Boolean = false
+    private var isRightEdge: Boolean = false
+    private var isTopEdge: Boolean = false
+    private var isBottomEdge: Boolean = false
+
+    private var lastX: Float = 0f
+    private var lastY: Float = 0f
+
+    private val gestureDetector: GestureDetector
+    private var isLongPressed: Boolean = false
+
+    init {
+        isClickable = true
+
+        gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onLongPress(e: MotionEvent) {
+                // Handle long press here
+                // You can add your code to perform actions only on long press
+                // For example, you can call a method like handleLongPress()
+                isLongPressed = true
+                setBackgroundColor(Color.RED)
+                onTouchEvent(e)
+            }
+        })
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(ev) || super.onInterceptTouchEvent(ev)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        if (!isLongPressed) {
+            return true
+        }
+
+        var newLeft = left
+        var newTop = top
+        var newRight = right
+        var newBottom = bottom
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                startX = event.x
+                startY = event.y
+
+                val viewWidth = width
+                val viewHeight = height
+
+                isLeftEdge = startX <= (viewWidth * 0.2)
+                isRightEdge = startX >= (viewWidth * 0.8)
+                isTopEdge = startY <= (viewHeight * 0.2)
+                isBottomEdge = startY >= (viewHeight * 0.8)
+
+                if (isLeftEdge) {
+                    Log.d(TAG, "ACTION_DOWN - Left edge touched initially")
+                } else if (isRightEdge) {
+                    Log.d(TAG, "ACTION_DOWN - Right edge touched initially")
+                }
+
+                if (isTopEdge) {
+                    Log.d(TAG, "ACTION_DOWN - Top edge touched initially")
+                } else if (isBottomEdge) {
+                    Log.d(TAG, "ACTION_DOWN - Bottom edge touched initially")
+                }
+
+                lastX = event.rawX
+                lastY = event.rawY
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                val currentX = event.rawX
+                val currentY = event.rawY
+
+                val deltaX = currentX - lastX
+                val deltaY = currentY - lastY
+
+                if (isLeftEdge) {
+                    Log.d(TAG, "ACTION_MOVE - Left edge touched initially")
+                    newLeft = (left + deltaX).toInt()
+                    newRight = right
+                } else if (isRightEdge) {
+                    Log.d(TAG, "ACTION_MOVE - Right edge touched initially")
+                    newRight = (right + deltaX).toInt()
+                    newLeft = left
+                }
+
+                if (isTopEdge) {
+                    Log.d(TAG, "ACTION_MOVE - Top edge touched initially")
+                    newTop = (top + deltaY).toInt()
+                    newBottom = bottom
+                } else if (isBottomEdge) {
+                    Log.d(TAG, "ACTION_MOVE - Bottom edge touched initially")
+                    newBottom = (bottom + deltaY).toInt()
+                    newTop = top
+                }
+
+                // If none of the edges is selected, perform the general move logic
+                if (!isLeftEdge && !isRightEdge && !isTopEdge && !isBottomEdge) {
+                    newLeft = (left + deltaX).toInt()
+                    newTop = (top + deltaY).toInt()
+                    newRight = (right + deltaX).toInt()
+                    newBottom = (bottom + deltaY).toInt()
+                }
+
+                layout(newLeft, newTop, newRight, newBottom)
+
+                layout(newLeft, newTop, newRight, newBottom)
+
+                lastX = currentX
+                lastY = currentY
+            }
+
+            MotionEvent.ACTION_UP -> {
+                isLongPressed = false
+                setBackgroundColor(Color.CYAN)
+            }
+
+            else -> {
+                // Handle other MotionEvent cases if needed
+            }
+        }
+        return true
+    }
+}
+
+
+
+
+
